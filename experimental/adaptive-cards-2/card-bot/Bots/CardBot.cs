ef87a8070f2aab7f9fb627692c168b6a5e6f2051
baton17 <snehsajal@gmail.com>
2021-04-29 19:14:39 +0530

    Adding Card Refresh

--- experimental/adaptive-cards-2/card-bot/Bots/CardBot.cs
+++ experimental/adaptive-cards-2/card-bot/Bots/CardBot.cs
@@ -7,10 +7,11 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using AdaptiveCards;
+using Cache;
 using Microsoft.Bot.AdaptiveCards;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
-using Newtonsoft.Json;
+
 
 namespace Microsoft.BotBuilderSamples.Bots
 {
@@ -18,10 +19,11 @@ namespace Microsoft.BotBuilderSamples.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
+            object adaptiveCardData = getAdaptiveCardData(turnContext.Activity.From.Id);
             var adaptiveCardAttachment = new Attachment()
             {
                 ContentType = "application/vnd.microsoft.card.adaptive",
-                Content = new CardResource(Path.Combine(".", "Cards", "CardOne.json")).AsJObject(),
+                Content = new CardResource(Path.Combine(".", "Cards", "CardOne.json")).AsJObject(adaptiveCardData),
             };
 
             await turnContext.SendActivityAsync(MessageFactory.Attachment(adaptiveCardAttachment, "Hello " + turnContext.Activity.From.Name + "!"), cancellationToken);
@@ -34,15 +36,21 @@ namespace Microsoft.BotBuilderSamples.Bots
                 try
                 {
                     AdaptiveCardInvoke request = AdaptiveCardInvokeValidator.ValidateRequest(turnContext);
-
-                    if (request.Action.Verb == "click")
+                    if (request.Action.Verb == "send")
                     {
-                        var responseBody = await ProcessClick();
+                        CardDB.updateCardData(request);
+                        var responseBody = await ProcessSend(turnContext);
                         return CreateInvokeResponse(HttpStatusCode.OK, responseBody);
                     }
-                    else if (request.Action.Verb == "back")
+                    else if (request.Action.Verb == "testAgain")
                     {
-                        var responseBody = await ProcessBack();
+                        CardDB.updateCardData(request);
+                        var responseBody = await ProcessTestAgain(turnContext);
+                        return CreateInvokeResponse(HttpStatusCode.OK, responseBody);
+                    }
+                    else if (request.Action.Verb == "refresh")
+                    {
+                        var responseBody = await ProcessRefresh(turnContext);
                         return CreateInvokeResponse(HttpStatusCode.OK, responseBody);
                     }
                     else
@@ -59,24 +67,49 @@ namespace Microsoft.BotBuilderSamples.Bots
             return null;
         }
 
-        private Task<AdaptiveCardInvokeResponse> ProcessClick()
+        private Task<AdaptiveCardInvokeResponse> ProcessSend(ITurnContext<IInvokeActivity> turnContext)
         {
+            object adaptiveCardData = getAdaptiveCardData(turnContext.Activity.From.Id);
             return Task.FromResult(new AdaptiveCardInvokeResponse()
             {
                 StatusCode = 200,
                 Type = AdaptiveCard.ContentType,
-                Value = new CardResource(Path.Combine(".", "Cards", "CardTwo.json")).AsJObject()
+                Value = new CardResource(Path.Combine(".", "Cards", "CardTwo.json")).AsJObject(adaptiveCardData)
             });
         }
 
-        private Task<AdaptiveCardInvokeResponse> ProcessBack()
+        private Task<AdaptiveCardInvokeResponse> ProcessTestAgain(ITurnContext<IInvokeActivity> turnContext)
         {
+            object adaptiveCardData = getAdaptiveCardData(turnContext.Activity.From.Id);
             return Task.FromResult(new AdaptiveCardInvokeResponse()
             {
                 StatusCode = 200,
                 Type = AdaptiveCard.ContentType,
-                Value = new CardResource(Path.Combine(".", "Cards", "CardOne.json")).AsJObject()
+                Value = new CardResource(Path.Combine(".", "Cards", "CardOne.json")).AsJObject(adaptiveCardData)
             });
+        }
+
+        private Task<AdaptiveCardInvokeResponse> ProcessRefresh(ITurnContext<IInvokeActivity> turnContext)
+        {
+            object adaptiveCardData = getAdaptiveCardData(turnContext.Activity.From.Id);
+            string adaptiveCardFileName = CardDB.getCurrentFileName();
+            return Task.FromResult(new AdaptiveCardInvokeResponse()
+            {
+                StatusCode = 200,
+                Type = AdaptiveCard.ContentType,
+                Value = new CardResource(Path.Combine(".", "Cards", adaptiveCardFileName)).AsJObject(adaptiveCardData)
+            });
+        }
+
+        private object getAdaptiveCardData(string userId)
+        {
+            object CardData = new
+            {
+                CardId = CardDB.getCardId(),
+                Text = CardDB.getCardText(),
+                UserId = new string[] { userId },
+            };
+            return CardData;
         }
 
         private static InvokeResponse CreateInvokeResponse(HttpStatusCode statusCode, object body = null)
---
+++ experimental/adaptive-cards-2/card-bot/Cache/CardDB.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.AdaptiveCards;
+using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices;
+
+namespace Cache
+{
+    public enum CardState
+    {
+        EnterText,
+        DisplayText
+    }
+    public class CardDB
+    {
+        private static string cardText = "";
+        private static CardState cardState = CardState.EnterText;
+        private static string cardId = "";
+        public static void updateCardData(AdaptiveCardInvoke request)
+        {
+            object data = request.Action.Data;
+            if (request.Action.Verb == "send")
+            {
+                cardText = JObject.FromObject(data)["text"]?.ToString();
+                cardState = CardState.DisplayText;
+            }
+            else if (request.Action.Verb == "testAgain")
+            {
+                cardText = "";
+                cardState = CardState.EnterText;
+            }
+            else
+                return;
+        }
+
+        public static string getCardText()
+        {
+            return cardText;
+        }
+
+        public static string getCardId()
+        {
+            if (cardId == "")
+                cardId = generateGUID();
+            return cardId;
+        }
+
+        private static string generateGUID()
+        {
+            return Marshal.GenerateGuidForType(typeof(string)).ToString();
+        }
+
+        public static string getCurrentFileName()
+        {
+            if (cardState == CardState.EnterText)
+                return "CardOne.json";
+            return "CardTwo.json";
+        }
+    }
+}
